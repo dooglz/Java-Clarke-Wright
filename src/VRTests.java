0be10347474a2130311b04e8c168141f040712a8
Sam Serrels <doogly1@gmail.com>
2014-10-30 09:59:56 +0000

    directory structure

--- src/VRTests.java
+++ src/VRTests.java
@@ -4,7 +4,8 @@ import java.io.*;
 public class VRTests {
 
 	public static void main(String[] args)throws Exception {
-		String dir = "tests/";
+		String problemdir = "tests/";
+		String outdir = "output/";
 		String [] shouldPass = {
 				"rand00010",
 				"rand00020",
@@ -33,39 +34,39 @@ public class VRTests {
 		System.out.println("\nShould Pass");
 		System.out.println("Problem     \tSoln\tSize\tCost\tValid");
 		for (String base:shouldPass){
-			VRProblem vrp = new VRProblem(dir+base+"prob.csv");
+			VRProblem vrp = new VRProblem(problemdir+base+"prob.csv");
 			VRSolution vrs = new VRSolution(vrp);
 
 			//Create a new solution using our poor algorithm
 			vrs.oneRoutePerCustomerSolution();
 
 			System.out.printf("%s\t%s\t%d\t%.0f\t%s\n",base,"dumb",vrp.size(),vrs.solnCost(),vrs.verify());
-			vrs.writeSVG(base+"prob.svg",base+"dmsn.svg");
-			if (new File(dir+base+"cwsn.csv").exists()){
-				vrs.readIn(dir+base+"cwsn.csv");
+			vrs.writeSVG(outdir+base+"prob.svg",outdir+base+"dmsn.svg");
+			if (new File(problemdir+base+"cwsn.csv").exists()){
+				vrs.readIn(problemdir+base+"cwsn.csv");
 
 				//Print out results of costing and verifying the solution
 				System.out.printf("%s\t%s\t%d\t%.0f\t%s\n",base,"CW",vrp.size(),vrs.solnCost(),vrs.verify());
 				
 				//Write the SVG file
-				vrs.writeSVG(dir+base+"prob.svg",base+"cwsn.svg");
+				vrs.writeSVG(outdir+base+"prob.svg",outdir+base+"cwsn.svg");
 			}
 		}
 		System.out.println("\nShould Fail");
 		System.out.println("Problem\tSolution\tSize\tCost\tValid");
 		for (String b:shouldFail){
-			VRProblem vrp = new VRProblem(dir+b+"prob.csv");
+			VRProblem vrp = new VRProblem(problemdir+b+"prob.csv");
 			VRSolution vrs = new VRSolution(vrp);
-			if (new File(dir+b+"soln.csv").exists()){
+			if (new File(problemdir+b+"soln.csv").exists()){
 
 				//Read an existing solution file
-				vrs.readIn(dir+b+"soln.csv");
+				vrs.readIn(problemdir+b+"soln.csv");
 
 				//Print out results of costing and verifying the solution
 				System.out.printf("%s\t%s\t%d\t%.0f\t%s\n",b,b,vrp.size(),vrs.solnCost(),vrs.verify());
 			
 				//Write the SVG file
-				vrs.writeSVG(dir+b+"prob.svg", b+"soln.svg");
+				vrs.writeSVG(outdir+b+"prob.svg", outdir+b+"soln.svg");
 			}
 		}
 	}
